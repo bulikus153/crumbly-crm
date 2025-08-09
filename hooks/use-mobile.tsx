18b3858a4f926b5885691f01856b81bc23f244ee
Bulikus <88974788+bulikus153@users.noreply.github.com>
2025-08-09 15:46:02 +0300

    fix: add media query listener fallback

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -10,9 +10,22 @@ export function useIsMobile() {
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
+    if (typeof mql.addEventListener === "function") {
+      mql.addEventListener("change", onChange)
+    } else {
+      // Safari < 14 support
+      // @ts-ignore - addListener exists on older browsers
+      mql.addListener(onChange)
+    }
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    return () => {
+      if (typeof mql.removeEventListener === "function") {
+        mql.removeEventListener("change", onChange)
+      } else {
+        // @ts-ignore - removeListener exists on older browsers
+        mql.removeListener(onChange)
+      }
+    }
   }, [])
 
   return !!isMobile
